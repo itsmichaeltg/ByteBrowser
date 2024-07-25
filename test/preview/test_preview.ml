e3ba570e8b09e6f24cd0f422a7165de2d809aceb
Jity Woldemichael <jity@mit.edu>
2024-07-25 17:48:06 +0000

    fix preview expect

--- test/preview/test_preview.ml
+++ test/preview/test_preview.ml
@@ -1,11 +1,15 @@
 open! Core
 open! File_manager_lib
 
-let%expect_test "navigate-left" =
+let%expect_test "preview" =
   let file = "/home/ubuntu/jsip-final-project/bin/main.ml" in
   let num_lines = 5 in
   print_endline (Preview.preview file ~num_lines);
   [%expect
     {|
+    open! Core
+    open! File_manager_lib
+
+    let () = Command_unix.run File_manager.command
     |}]
 ;;
