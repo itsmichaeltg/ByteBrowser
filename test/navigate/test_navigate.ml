22271208f386ae25e63ccb3bb53b0f97bd20822e
Jity Woldemichael <jity@mit.edu>
2024-07-25 14:03:20 +0000

    full test

--- test/navigate/test_navigate.ml
+++ test/navigate/test_navigate.ml
@@ -87,6 +87,46 @@ let%expect_test "navigate-up" =
   let model = {Navigate.State.choices = {matrix = mat}; current_path = "/home/home_dir2"; origin = "/home"} in
   let new_model = Navigate.State.get_updated_model_for_up model in
   print_endline (Visualize_helper.visualize new_model.choices.matrix ~current_directory:"/home" ~path_to_be_underlined:new_model.current_path);
+  let newer_model = Navigate.State.get_updated_model_for_up new_model in
+  print_endline (Visualize_helper.visualize newer_model.choices.matrix ~current_directory:"/home" ~path_to_be_underlined:newer_model.current_path);
+  [%expect
+    {|
+    .
+    [0m[0m|__ 📁[;0;36mhome
+    [0m  [0m|__ 📁[;0;36mhome_dir1
+    [0m    [0m|__ 📁[;0;36mchild1
+    [0m      [0m|__ [;0;35m.gitignore
+    [0m    [0m|__ [;0mchild2
+    [0m  [0m|__ 📁[;0;4;36mhome_dir2
+    .
+    [0m[0m|__ 📁[;0;36mhome
+    [0m  [0m|__ 📁[;0;4;36mhome_dir1
+    [0m    [0m|__ 📁[;0;36mchild1
+    [0m      [0m|__ [;0;35m.gitignore
+    [0m    [0m|__ [;0mchild2
+    [0m  [0m|__ 📁[;0;36mhome_dir2
+    .
+    [0m[0m|__ 📁[;0;36mhome
+    [0m  [0m|__ 📁[;0;4;36mhome_dir1
+    [0m    [0m|__ 📁[;0;36mchild1
+    [0m      [0m|__ [;0;35m.gitignore
+    [0m    [0m|__ [;0mchild2
+    [0m  [0m|__ 📁[;0;36mhome_dir2
+    |}]
+;;
+
+let%expect_test "navigate-down" =
+  let mat = Hashtbl.create (module String) in
+  Hashtbl.add_exn mat ~key:"/home" ~data:[ "/home/home_dir1"; "/home/home_dir2" ];
+  Hashtbl.add_exn mat ~key:"/home/home_dir1" ~data:[ "/home/home_dir1/child1"; "/home/home_dir1/child2" ];
+  Hashtbl.add_exn mat ~key:"/home/home_dir2" ~data:[];
+  Hashtbl.add_exn mat ~key:"/home/home_dir1/child1" ~data:[ "/home/home_dir1/child1/.gitignore" ];
+  print_endline (Visualize_helper.visualize mat ~current_directory:"/home" ~path_to_be_underlined:"/home/home_dir1");
+  let model = {Navigate.State.choices = {matrix = mat}; current_path = "/home/home_dir1"; origin = "/home"} in
+  let new_model = Navigate.State.get_updated_model_for_down model in
+  print_endline (Visualize_helper.visualize new_model.choices.matrix ~current_directory:"/home" ~path_to_be_underlined:new_model.current_path);
+  let newer_model = Navigate.State.get_updated_model_for_down new_model in
+  print_endline (Visualize_helper.visualize newer_model.choices.matrix ~current_directory:"/home" ~path_to_be_underlined:newer_model.current_path);
   [%expect
     {||}]
 ;;
