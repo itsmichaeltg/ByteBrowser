1f71551ace2b50c309144d4982f3a6fe2993ef96
Jity Woldemichael <jity@mit.edu>
2024-08-01 20:46:47 +0000

    fix commit

--- tests/query/test_query.ml
+++ tests/query/test_query.ml
@@ -3,8 +3,8 @@ open! File_manager_lib
 
 let%expect_test "summary" =
   let origin = "/home/ubuntu/jsip-final-project/src" in
-  let tree = Visualize.Adjacency_matrix.create () |> Visualize.Adjacency_matrix.get_adjacency_matrix ~sort:true ~show_hidden:false ~origin ~max_depth:100 in
-  let summary = Summary.generate tree.matrix origin in
+  let tree = Matrix.create () |> Matrix.get_adjacency_matrix ~sort:true ~show_hidden:false ~origin ~max_depth:100 in
+  let summary = Summary.generate tree origin in
   let result = Querying.query "q: what does the visualize helper do exactly?" ~info:summary in
   print_endline result;
   [%expect {||}]
--- tests/summary/test_summary.ml
+++ tests/summary/test_summary.ml
@@ -3,7 +3,7 @@ open! File_manager_lib
 
 let%expect_test "summary" =
   let origin = "/home/ubuntu/jsip-final-project/src" in
-  let tree = Visualize.Adjacency_matrix.create () |> Visualize.Adjacency_matrix.get_adjacency_matrix ~sort:true ~show_hidden:false ~origin ~max_depth:100 in
-  let result = Summary.generate tree.matrix origin in
+  let tree = Matrix.create () |> Matrix.get_adjacency_matrix ~sort:true ~show_hidden:false ~origin ~max_depth:100 in
+  let result = Summary.generate tree origin in
   print_endline result;
   [%expect {||}]
