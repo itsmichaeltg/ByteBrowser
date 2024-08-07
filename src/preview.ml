3ca68d8130fa6f50c482aa21f4e6f0f88dda2c3e
Jity Woldemichael <jity@mit.edu>
2024-08-07 15:16:01 +0000

    fix bg for preview

--- src/preview.ml
+++ src/preview.ml
@@ -1,28 +1,28 @@
 open! Core
 
 let apply_syntax_highlight lines = lines
+let apply_bg line = "\x1b[0;48;5;23m" ^ line
+let transform_line line = Styles.normalize_string line |> apply_bg
 
 let get_lines path ~num_lines =
-  let lst = In_channel.read_lines path in
+  let lst = List.append [ "" ] (In_channel.read_lines path) in
   let relevant_lines = List.slice lst 0 (min num_lines (List.length lst)) in
-  List.map relevant_lines ~f:Styles.normalize_string
+  List.map relevant_lines ~f:transform_line
 ;;
 
+let apply_styles_to_title title = "\x1b[0;22;3;4;48;5;23;38;5;118m" ^ title
+
 let get_title path =
-  Styles.normalize_string ("viewing " ^ Matrix.get_name path)
+  apply_styles_to_title
+    (Styles.normalize_string ("viewing " ^ Matrix.get_name path))
 ;;
 
 let apply_outer_styles path ~content =
   (* let extension = Matrix.get_extension_of_file path in *)
-  let styles = { Styles.styles = [ "0"; "48"; "5"; "129" ] } in
-  let title = get_title path in
-  let space_between_title_and_content = Styles.get_normalized_new_line () in
-  let content_with_colors =
-    Styles.apply_style
-      styles
-      ~apply_to:(title ^ space_between_title_and_content ^ content)
-  in
-  Styles.apply_borders content_with_colors ^ "\x1b[0m" ^ "\n"
+  Styles.apply_borders (get_title path)
+  ^ Styles.apply_borders content
+  ^ "\x1b[0m"
+  ^ "\n"
 ;;
 
 let concat_lines lines = String.concat lines ~sep:"\n"
