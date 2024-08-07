bb170f76dc68e412ddea8da5ec16f991a57d2213
Jity Woldemichael <jity@mit.edu>
2024-08-07 16:15:57 +0000

    init syntax highlighter

--- src/preview.ml
+++ src/preview.ml
@@ -1,13 +1,22 @@
 open! Core
 
-let apply_syntax_highlight lines = lines
+let path_to_write_to = "/home/ubuntu/jsip-final-project/bin/code_to_be_highlighted.txt"
+let path_to_script = "/home/ubuntu/jsip-final-project/src/syntax_highlighting_script.py"
+let path_to_read_from = "/home/ubuntu/jsip-final-project/bin/highlighted_code.txt"
+
+let apply_syntax_highlight (lines : string list) path =
+  Out_channel.write_lines path_to_write_to lines;
+  let file_name = Matrix.get_name path in
+  let _ = Sys_unix.command ("python3 " ^ path_to_script ^ " " ^ file_name) in
+  In_channel.read_lines path_to_read_from
+
 let apply_bg line = "\x1b[0;48;5;23m" ^ line
-let transform_line line = Styles.normalize_string line |> apply_bg
+(* let transform_line line = Styles.normalize_string line |> apply_bg *)
 
 let get_lines path ~num_lines =
   let lst = List.append [ "" ] (In_channel.read_lines path) in
   let relevant_lines = List.slice lst 0 (min num_lines (List.length lst)) in
-  List.map relevant_lines ~f:transform_line
+  List.map relevant_lines ~f:Styles.normalize_string
 ;;
 
 let apply_styles_to_title title = "\x1b[0;22;3;4;48;5;23;38;5;118m" ^ title
@@ -29,7 +38,7 @@ let concat_lines lines = String.concat lines ~sep:"\n"
 
 let preview path ~num_lines =
   let lines = get_lines path ~num_lines in
-  let formatted_lines = apply_syntax_highlight lines in
+  let formatted_lines = apply_syntax_highlight lines path in
   let concatted_lines = concat_lines formatted_lines in
   apply_outer_styles path ~content:concatted_lines
 ;;
--- tests/preview/test_preview.ml
+++ tests/preview/test_preview.ml
@@ -6,11 +6,11 @@ let%expect_test "preview" =
   let num_lines = 100 in
   print_endline (Preview.preview file ~num_lines);
   [%expect {|
-    [;0;48;5;129mviewing main.ml
-
-    [0m>>open! Core                                                                                          [0m<<
-    [0m>>open! File_manager_lib                                                                              [0m<<
-    [0m>>                                                                                                    [0m<<
-    [0m>>let () = Command_unix.run Navigate.command                                                          [0m<<[0m
+    [0m>>  [0;22;3;4;48;5;23;38;5;118mviewing main.ml                                                                                     [0m  <<
+    [0m>>  [38;5;15m                                                                                                    [39m[0m  <<
+    [0m>>  [38;5;15mopen[39m[38;5;198;48;5;233m![39;49m[38;5;15m [39m[38;5;15mCore[39m[38;5;15m                                                                                          [39m[0m  <<
+    [0m>>  [38;5;15mopen[39m[38;5;198;48;5;233m![39;49m[38;5;15m [39m[38;5;15mFile_manager_lib[39m[38;5;15m                                                                              [39m[0m  <<
+    [0m>>  [38;5;15m                                                                                                    [39m[0m  <<
+    [0m>>  [38;5;15mlet[39m[38;5;15m [39m[38;5;15m([39m[38;5;15m)[39m[38;5;15m [39m[38;5;204m=[39m[38;5;15m [39m[38;5;15mCommand_unix[39m[38;5;204m.[39m[38;5;15mrun[39m[38;5;15m [39m[38;5;15mNavigate[39m[38;5;204m.[39m[38;5;15mcommand[39m[38;5;15m                                                          [39m[0m  <<[0m
     |}]
 ;;
